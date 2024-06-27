a2103b09bd505b4ff91affb7dcc9e84cc7cf4ac3
Matthew M-B <matthew.mb@getjobber.com>
2024-06-27 05:29:21 -0400

    Update collection validation

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,14 +2,15 @@ import { z, defineCollection } from "astro:content";
 
 const questionCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    author: z.string(),
-    path: z.string(),
-    solution: z.string().optional(),
-    generator: z.string().optional(),
-    tags: z.array(z.string()),
-  }),
+  // TODO: we should enforce this schema
+  // schema: z.object({
+  //   title: z.string(),
+  //   author: z.string(),
+  //   path: z.string(),
+  //   solution: z.string().optional(),
+  //   generator: z.string().optional(),
+  //   tags: z.array(z.string()),
+  // }),
 });
 
 export const collections = {
--- src/content/questions/comp2804/midterm-fall-2013/1/solution.md
+++ src/content/questions/comp2804/midterm-fall-2013/1/solution.md
@@ -1,13 +1,3 @@
----
-title: TBD
-path: comp2804/midterm-fall-2013/1
-author: Pat Morin
-solution: comp2804/midterm-fall-2013/1/solution.tex
-generator: comp2804/midterm-fall-2013/1/generator.ts
-tags:
-  - comp2804
----
-
 ## Solution.md (markdown)
 
 $\int_0^\infty = \frac{1}{x^2+1} \;dx=\frac{\pi}{2}$
