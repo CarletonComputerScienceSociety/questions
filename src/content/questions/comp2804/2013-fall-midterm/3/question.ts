92d73efde8a0699e2e4a6ba454da49fc6a95f954
John Lu <john.lu10212004@gmail.com>
2024-07-17 17:29:07 -0400

    fixed prettier error

--- src/content/questions/comp2804/2013-fall-midterm/3/question.ts
+++ src/content/questions/comp2804/2013-fall-midterm/3/question.ts
@@ -4,8 +4,8 @@ export const question: MultipleChoiceQuestion = {
   body: "A password consists of 6 or 7 characters, each character being an uppercase or a lowercase letter. A password must contain at least one uppercase letter. How many passwords are there?",
   options: [
     { label: "$52^{6} + 52^{7}$", correct: false },
-    { label: "$26 \cdot 52^{5} + 26 \cdot 52^{6}$", correct: true },
+    { label: "$26 cdot 52^{5} + 26 cdot 52^{6}$", correct: true },
     { label: "$52^{6} + 52^{7} - 26^{6} - 26^{7}$", correct: false },
-    { label: "None of the above." correct: false },
+    { label: "None of the above.", correct: false },
   ],
 };
