21d4b99c657155b487dec40380d212cda474aa07
Matthew M-B <matthew.mb@getjobber.com>
2024-08-02 03:11:15 -0400

    Add question mark

--- src/content/questions/comp2804/2022-winter-final/1/question.ts
+++ src/content/questions/comp2804/2022-winter-final/1/question.ts
@@ -1,6 +1,6 @@
 import type { MultipleChoiceQuestion } from "@common/MultipleChoiceQuestionGenerator";
 
-const body = String.raw`Consider strings of length $70$, in which each character is one of the characters $a,b,c,d,e$.  How many such strings have exactly $10$ letters $e$`;
+const body = String.raw`Consider strings of length $70$, in which each character is one of the characters $a,b,c,d,e$.  How many such strings have exactly $10$ letters $e$?`;
 
 const label1 = String.raw`$\binom{70}{5}\cdot 5^{60}$`;
 const label2 = String.raw`$\binom{70}{10}\cdot 5^{60}$`;
