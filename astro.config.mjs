111d741becf17aef759600aa3f949bec90cc03ef
John Lu <87673068+JohnLu2004@users.noreply.github.com>
2024-08-03 17:09:09 -0400

    Add page layout updates and pagination (#20)
    * added pagination
    
    * Add page layout updates
    
    ---------
    
    Co-authored-by: Matthew M-B <matthew.mb@getjobber.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   // More configs
-  integrations: [
-    svelte(),
-    mdx()
-  ],
-  site: 'https://questions.carletoncomputerscience.ca',
+  integrations: [svelte(), mdx()],
+  site: "https://questions.carletoncomputerscience.ca",
   server: { port: 10000, host: true },
   devToolbar: {
-    enabled: false
-  }
+    enabled: false,
+  },
+  redirects: {
+    "/evaluations": "/evaluations/1",
+    "/" : "/comp2804",
+  },
 });
